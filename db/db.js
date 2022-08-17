d2be3bd9a729a112b85d2d31ffe32a37fa6f1bf9
dougMR <1236472+dougMR@users.noreply.github.com>
2022-08-17 18:51:44 -0400

    set db for working both heroku and localhost

--- db/db.js
+++ db/db.js
@@ -1,9 +1,31 @@
 // #3 setup DB models
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://dougroussin@localhost:5432/blog", {
-    logging: false,
-});
+let options = {};
+// if on heroku, there will be process.env, with property DATABASE_URL
+let databaseURL = process.env.DATABASE_URL;
+if (!databaseURL) {
+    // we're on localhost
+    databaseURL = "postgres://dougroussin@localhost:5432/blog";
+    options = {
+        logging: false,
+    };
+} else {
+    // we're not on localhost
+    options = {
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            }
+        }
+    };
+}
+const db = new Sequelize(databaseURL, options);
+// const db = new Sequelize("postgres://dougroussin@localhost:5432/blog", {
+//     logging: false,
+// });
 const User = require("./User")(db);
 const Post = require("./Post")(db);
 
